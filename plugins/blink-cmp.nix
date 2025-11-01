e3da0a4723543d1b9c66b51472b75496cdaf9fd5
Levizor <levizor@disroot.org>
2025-11-01 01:39:45 +0100

    update: configure my blink-cmp

--- plugins/blink-cmp.nix
+++ plugins/blink-cmp.nix
@@ -36,30 +36,14 @@ in
             "fallback"
           ];
           "<C-space>" = [
-            (
-              # lua
-              mkRaw ''
-                function(cmp)
-                  local ok,_ = pcall(require, "copilot")
-                    if ok then
-                      vim.g.copilot_no_tab_map = true
-                      vim.g.copilot_assume_mapped = true
-                      vim.g.copilot_tab_fallback = ""
-
-                      local suggestion = require("copilot.suggestion")
-                      if suggestion.is_visible() then
-                        suggestion.accept()
-                      else
-                        if cmp.snippet_active() then
-                        return cmp.select_and_accept()
-                        else
-                        return cmp.accept()
-                        end
-                      end
-                    end
-                  end
-              ''
-            )
+            "show"
+            "show_documentation"
+            "hide_documentation"
+            "fallback"
+          ];
+          "<cr>" = [
+            "select_and_accept"
+            "fallback"
           ];
         };
       };
